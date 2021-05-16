5d32263bee4a9a92838d8fc9111d9b1bfb4e234b
Will <kode15333@gmail.com>
2021-05-16 20:45:18 +0900

    study: custom tag how to use attribute

--- chapter4/step1/componenet/helloword.js
+++ chapter4/step1/componenet/helloword.js
@@ -1,7 +1,21 @@
+const DEFAULT_COLOR = 'black';
 export default class Helloword extends HTMLElement {
+  get color () {
+    return this.getAttribute('color') || DEFAULT_COLOR
+  }
+
+  set color (value) {
+    this.setAttribute('color', value);
+  }
   connectedCallback(){
     window.requestAnimationFrame(() => {
-      this.innerHTML = '<div>HELLO WORLD</div>'
+      const div = document.createElement('div');
+      div.innerHTML = '<div>HELLO WORLD</div>'
+
+      div.style.color = this.color
+
+
+      this.appendChild(div)
     })
   }
 }
