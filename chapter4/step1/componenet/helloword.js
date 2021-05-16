b0030268b65588b34fbb084a2954b1a102bccc39
Will <kode15333@gmail.com>
2021-05-16 20:52:25 +0900

    study: how to observedAttribues And change

--- chapter4/step1/componenet/helloword.js
+++ chapter4/step1/componenet/helloword.js
@@ -1,21 +1,35 @@
-const DEFAULT_COLOR = 'black';
-export default class Helloword extends HTMLElement {
+
+const DEFAULT_COLOR = 'black'
+
+export default class HelloWorld extends HTMLElement {
+  static get observedAttributes () {
+    return ['color']
+  }
+
   get color () {
     return this.getAttribute('color') || DEFAULT_COLOR
   }
 
   set color (value) {
-    this.setAttribute('color', value);
+    this.setAttribute('color', value)
   }
-  connectedCallback(){
+
+  attributeChangedCallback (name, oldValue, newValue) {
+    if (!this.div) {
+      return
+    }
+
+    if (name === 'color') {
+      this.div.style.color = newValue
+    }
+  }
+
+  connectedCallback () {
     window.requestAnimationFrame(() => {
-      const div = document.createElement('div');
-      div.innerHTML = '<div>HELLO WORLD</div>'
-
-      div.style.color = this.color
-
-
-      this.appendChild(div)
+      this.div = document.createElement('div')
+      this.div.textContent = 'Hello World!'
+      this.div.style.color = this.color
+      this.appendChild(this.div)
     })
   }
 }
--- chapter4/step1/index.js
+++ chapter4/step1/index.js
@@ -1,5 +1,18 @@
-import Helloword from './componenet/helloword.js';
+import Helloword from './componenet/helloword.js'
 
 window
-.customElements
-.define('hello-world', Helloword)
+  .customElements
+  .define('hello-world', Helloword)
+
+const changeColorTo = color => {
+  document
+    .querySelectorAll('hello-world')
+    .forEach(helloworld => {
+    helloworld.color = color
+  })
+}
+document
+  .querySelector('button')
+  .addEventListener('click', () => {
+    changeColorTo('blue');
+  })
