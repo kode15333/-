1e8a879bfd3dce9ea234c5f9f5c0a4e648853c95
Will <kode15333@gmail.com>
2021-05-29 20:53:05 +0900

    model test code

--- chapter7/step1/model/model.js
+++ chapter7/step1/model/model.js
@@ -2,13 +2,32 @@ const cloneDeep = x => {
   return JSON.parse(JSON.stringify(x))
 }
 
+const freeze = x => Object.freeze(cloneDeep(x));
+
 const INITIAL_STATE = {
   todos: [],
   currentFilter: 'ALL'
 }
 
 export default (initialState = INITIAL_STATE) => {
-  const state = cloneDeep(initialState)
+  const state = cloneDeep(initialState);
+
+  let listeners = [];
+
+  const addChangeListener = listener => {
+    listeners.push(listener);
+
+    listener(freeze(state));
+
+    return () => {
+      listeners = listeners.filter( l => l !== listener);
+    }
+  }
+
+  const invokeListeners = () => {
+    const data = freeze(state);
+    listeners.forEach(l => l(data));
+  }
 
   const getState = () => {
     return Object.freeze(cloneDeep(state))
@@ -19,6 +38,8 @@ export default (initialState = INITIAL_STATE) => {
     state.todos.push({
       text, completed: false
     })
+
+    invokeListeners();
   }
   const updateItem = (index, text) => {
     if (!text || index < 0 ) return;
@@ -26,6 +47,8 @@ export default (initialState = INITIAL_STATE) => {
     if(!state.todos[index]) return;
 
     state.todos[index].text = text;
+
+    invokeListeners();
   }
   const deleteItem = (index) => {
     if(index < 0) return;
@@ -33,6 +56,8 @@ export default (initialState = INITIAL_STATE) => {
     if(!state.todos[index]) return;
 
     state.todos.splice(index, 1);
+
+    invokeListeners();
   }
   const toggleITemCompleted = (index) => {
     if(index < 0) return;
@@ -40,17 +65,25 @@ export default (initialState = INITIAL_STATE) => {
     if(!state.todos[index]) return;
 
     state.todos[index].completed = !state.todos[index].completed;
+
+    invokeListeners();
   }
   const completeAll = () => {
     state.todos.forEach(t =>{
       t.completed = true;
     })
+    invokeListeners();
+
   }
   const clearCompleted = () => {
     state.todos = state.todos.filter(t => !t.completed);
+    invokeListeners();
+
   }
   const changeFilter = (filter) => {
     state.currentFilter = filter;
+    invokeListeners();
+
   }
 
   return {
@@ -61,6 +94,6 @@ export default (initialState = INITIAL_STATE) => {
     completeAll,
     clearCompleted,
     changeFilter,
-    getState
+    addChangeListener
   }
 }
